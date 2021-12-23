ee78a14f780bc2e0db6687f40ca77f6589c52257
Tomas Gonzalez <r.tomas.gonzalez01@gmail.com>
2021-12-23 14:05:56 -0600

    working with express
    Learned how middleware works, and how we go from one middleware to the next (middleware basics). Learned about bodyparser and its functionality. Learned about path, and how we join different paths (linking to different html docs). Learned about statically serving files. Tools used: Router, body-parser, path, express.static , rootDir, .use, .sendFile

---
+++ routes/shop.js
@@ -0,0 +1,12 @@
+// routes that are used by the public user
+
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
